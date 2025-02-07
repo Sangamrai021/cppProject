b5a19df49b17e7e4b1252da6c93a79d5a449a8f1
Sangamrai021 <raisangam021@gmail.com>
2025-02-07 17:51:01 +0545

    test123

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int n,m,sum;
+	cout<<" \nFinding the sum of numbers\n ";
+	cout<<"\nEnter First & Second Number: \n";
+	cin>>n >>m;
+	sum = n+m;
+	cout<<"The Sum of " << n <<" and "<< m <<" is "<< sum <<" "<<endl;
+	return 0; 
+}
